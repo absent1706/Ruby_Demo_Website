114aab97d61db566ba9e5ab0d83ad99f296583bd
absent1706 <litvinenko1706@gmail.com>
2014-01-09 20:40:44 +0200

    Сделал стилизацию по главе 5

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
-
+gem 'bootstrap-sass', '2.0.0'
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 DemoApp::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
-
+root to:'static_pages#home'
+match '/home', to: 'static_pages#home'
+match '/help', to: 'static_pages#help'
+match '/about', to: 'static_pages#about'
+match '/contact', to: 'static_pages#contact'
   resources :microposts
 
 
