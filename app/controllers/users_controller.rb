26063e6cc72982f2cdd109bb3421fc7d9cf2c680
absent1706 <litvinenko1706@gmail.com>
2014-01-17 20:37:55 +0200

    Начало главы 9.2.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
-  before_filter :find_user,:only=>[:edit,:show,:update]  
-  before_filter :check_authentification, only: [:edit, :update]
-  before_filter :check_user, only: [:edit, :update]
+  before_filter :find_user,:only=>[:edit,:show,:update]
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
   def new
     @user = User.new
   end
 
-  
+
   def create
     @user = User.new(params[:user])
-    
+
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
-      redirect_to @user
+      redirect_back_or @user
     else
       render 'edit'
     end
@@ -42,16 +42,22 @@ class UsersController < ApplicationController
 
   def find_user
     @user = User.find(params[:id])
-  end 
-
-  def check_authentification
-    #если юзер вообще не авторизован, отправляем его на страницу авторизации
-    redirect_to signin_url, notice: "Please sign in." unless signed_in?
   end
 
-  def check_user
-    #если юзер хочет отредактировать чужие данные, отправляем его на домашнюю страницу сайта
+
+  private
+
+  #если юзер вообще не авторизован, отправляем его на страницу авторизации
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  #если юзер хочет отредактировать чужие данные, отправляем его на домашнюю страницу сайта
+  def correct_user
     redirect_to(root_path) unless current_user?(@user)
   end
-  
+
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -48,6 +48,20 @@ describe "authorization" do
   subject { page }
   describe "for non-signed-in users" do
     let(:user) { FactoryGirl.create(:user) }
+    describe "when attempting to visit a protected page" do
+      before do
+        visit edit_user_path(user)
+        fill_in "Email",    with: user.email
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
+
+      describe "after signing in" do
+        it "should render the desired protected page" do
+          page.should have_selector('title', text: 'Edit user')
+        end
+      end
+    end
 
     describe "in the Users controller" do
 
@@ -61,21 +75,24 @@ describe "authorization" do
         specify { response.should redirect_to(signin_path) }
       end
     end
+
+
+
   end
 
   describe "as wrong user" do
-      let(:user) { FactoryGirl.create(:user) }
-      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-      before { sign_in user }
+    let(:user) { FactoryGirl.create(:user) }
+    let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+    before { sign_in user }
 
-      describe "visiting Users#edit page" do
-        before { visit edit_user_path(wrong_user) }
-        it { should_not have_selector('title', text: 'Edit user') }
-      end
-
-      describe "submitting a PUT request to the Users#update action" do
-        before { put user_path(wrong_user) }
-        specify { response.should redirect_to(root_path) }
-      end
+    describe "visiting Users#edit page" do
+      before { visit edit_user_path(wrong_user) }
+      it { should_not have_selector('title', text: 'Edit user') }
     end
+
+    describe "submitting a PUT request to the Users#update action" do
+      before { put user_path(wrong_user) }
+      specify { response.should redirect_to(root_path) }
+    end
+  end
 end
