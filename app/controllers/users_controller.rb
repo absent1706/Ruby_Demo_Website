53b4e6e333de4649cbd11559e3fbc8b4781ad00e
absent1706 <litvinenko1706@gmail.com>
2014-01-17 20:50:33 +0200

    Конец главы 9.2.3. Сделана дружелюбная переадресация(если неавторизованный юзер пытался зайти на страницу редактирования профиля, куда его не пустило, то после авторизации он перенаправляется на ту же страницу редактирования,куда ему и нужно было)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
-      redirect_back_or @user
+      redirect_to @user
     else
       render 'edit'
     end
@@ -51,7 +51,7 @@ class UsersController < ApplicationController
   def signed_in_user
     unless signed_in?
       store_location
-      redirect_to signin_url, notice: "Please sign in."
+      redirect_to signin_path, notice: "Please sign in."
     end
   end
 
