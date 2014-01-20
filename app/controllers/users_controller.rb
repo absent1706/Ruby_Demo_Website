0d82e91df3761fae0a4f4c3d3e61b73bcfd4703e
absent1706 <litvinenko1706@gmail.com>
2014-01-20 18:54:15 +0200

    Закончена часть 10.2. Микропосты пользователя теперь отображаются на странице его профиля. БД заполнена отличными фейковыми данными

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,10 +13,19 @@ describe "User pages" do
 
 	describe "profile page" do
 		let(:user) { FactoryGirl.create(:user) }
+		let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+		let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
 		before { visit user_path(user) }
 
 		it { should have_selector('h1',    text: user.name) }
 		it { should have_selector('title', text: user.name) }
+
+		describe "microposts" do
+			it { should have_content(m1.content) }
+			it { should have_content(m2.content) }
+			it { should have_content(user.microposts.count) }
+		end
 	end
 
 	describe "signup page" do
@@ -42,12 +51,6 @@ describe "User pages" do
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
 			end 	
-
-	#		describe "after saving the user" do
-				
-	#			click_button("Create my account")
-	#			it { should have_link('Sign out') }
-#			end
 
 
 		end
@@ -132,14 +135,14 @@ describe "User pages" do
 				it { should have_link('delete', href: user_path(User.first)) }
 				it "should be able to delete another user" do
 					expect do
-					 click_link('delete') 
+						click_link('delete') 
 					# click_button('OK')
-					end.to change(User, :count).by(-1)
-				end
-				it { should_not have_link('delete', href: user_path(admin)) }
+				end.to change(User, :count).by(-1)
 			end
+			it { should_not have_link('delete', href: user_path(admin)) }
 		end
-
-
 	end
+
+
+end
 end
