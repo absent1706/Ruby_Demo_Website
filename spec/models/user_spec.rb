289202912f7315f75870dfd7f3dbac0be86f5e29
absent1706 <litvinenko1706@gmail.com>
2014-01-21 16:03:53 +0200

    Конец 11.1. Теперь можно узнать, на кого юзер подписан и кто подписан на юзера

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,174 +2,182 @@ require 'spec_helper'
 
 describe User do
 
-	before do
-		@user = User.new(name: "Example User", email: "user@example.com",
-			password: "foobar", password_confirmation: "foobar")
-	end
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
-	subject { @user }
+  subject { @user }
 
-	it { should respond_to(:name) }
-	it { should respond_to(:email) }
-	it { should respond_to(:password_digest) }
-	it { should respond_to(:password) }
-	it { should respond_to(:password_confirmation) }
-	it { should respond_to(:remember_token) }
-	it { should respond_to(:authenticate) }
-	it { should respond_to(:admin) }
-	it { should respond_to(:microposts) }
-	it { should respond_to(:feed) }
-	it { should respond_to(:relationships) }
-	it { should respond_to(:followed_users) }
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+  it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
 
-	it { should be_valid }
-	it { should_not be_admin }
+  it { should be_valid }
+  it { should_not be_admin }
 
-	describe "with admin attribute set to 'true'" do
-		before do
-			@user.save!
-			@user.toggle!(:admin)
-		end
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
 
-		it { should be_admin }
-	end
-	
-	#Тесты, связанные с паролем
-	describe "when password is not present" do
-		before { @user.password = @user.password_confirmation = " " }
-		it { should_not be_valid }
-	end
+    it { should be_admin }
+  end
 
-	describe "when password doesn't match confirmation" do
-		before { @user.password_confirmation = "mismatch" }
-		it { should_not be_valid }
-	end
+  #Тесты, связанные с паролем
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
 
-	describe "when password confirmation is nil" do
-		before { @user.password_confirmation = nil }
-		it { should_not be_valid }
-	end
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
 
-	describe "return value of authenticate method" do
-		before { @user.save }
-		let(:found_user) { User.find_by_email(@user.email) }
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
 
-		describe "with valid password" do
-			it { should == found_user.authenticate(@user.password) }
-		end
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
 
-		describe "with invalid password" do
-			let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+    describe "with valid password" do
+      it { should == found_user.authenticate(@user.password) }
+    end
 
-			it { should_not == user_for_invalid_password }
-			specify { user_for_invalid_password.should be_false }
-		end
-	end
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
-	describe "with a password that's too short" do
-		before { @user.password = @user.password_confirmation = "a" * 5 }
-		it { should be_invalid }
-	end
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
+    end
+  end
 
-	#Конец тестов, связанных с паролем
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
 
-
-
-	describe "when name is not present" do
-		before { @user.name = " " }
-		it { should_not be_valid }
-	end
-
-	describe "when email format is invalid" do
-		it "should be invalid" do
-			addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
-			addresses.each do |invalid_address|
-				@user.email = invalid_address
-				@user.should_not be_valid
-			end
-		end
-	end
-
-	describe "when email format is valid" do
-		it "should be valid" do
-			addresses = %w[user@fo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-			addresses.each do |valid_address|
-				@user.email = valid_address
-				@user.should be_valid
-			end
-		end
-	end
-
-	describe "when email address is already taken" do
-		before do
-			user_with_same_email = @user.dup
-			user_with_same_email.save
-		end
-
-		it { should_not be_valid }
-	end
+  #Конец тестов, связанных с паролем
 
 
-	describe "remember token" do
-		before { @user.save }
-		its(:remember_token) { should_not be_blank }
-	end
-	
 
-	describe "micropost associations" do
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
 
-		before { @user.save }
-		let!(:older_micropost) do
-			FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
-		end
-		let!(:newer_micropost) do
-			FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
-		end
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end
+    end
+  end
 
-		it "should have the right microposts in the right order" do
-			@user.microposts.should == [newer_micropost, older_micropost]
-		end
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@fo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end
+    end
+  end
 
-		it "should destroy associated microposts" do
-			microposts = @user.microposts.dup
-			@user.destroy
-			microposts.should_not be_empty
-			microposts.each do |micropost|
-				Micropost.find_by_id(micropost.id).should be_nil
-			end
-		end
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
 
 
-		describe "status" do
-			let(:unfollowed_post) do
-				FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
-			end
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
-			its(:feed) { should include(newer_micropost) }
-			its(:feed) { should include(older_micropost) }
-			its(:feed) { should_not include(unfollowed_post) }
-		end
-		
-	end
 
-	#проверка того, что после соотв. операций юзер действительно следует за другим юзером (подписан на него)
-	describe "following" do
-		let(:other_user) { FactoryGirl.create(:user) }
-		before do
-			@user.save
-			@user.follow!(other_user)
-		end
+  describe "micropost associations" do
 
-		it { should be_following(other_user) }
-		its(:followed_users) { should include(other_user) }
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
 
-		#срабатывает ли отписка
-		describe "and unfollowing" do
-			before { @user.unfollow!(other_user) }
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
 
-			it { should_not be_following(other_user) }
-			its(:followed_users) { should_not include(other_user) }
-		end
-	end
-	
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.dup
+      @user.destroy
+      microposts.should_not be_empty
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
+
+  end
+
+  #проверка того, что после соотв. операций юзер действительно следует за другим юзером (подписан на него)
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    #в подписчиках other_user'a должен появиться только что подписавшийся user
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+
+    #срабатывает ли отписка
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+  end
+
 end
