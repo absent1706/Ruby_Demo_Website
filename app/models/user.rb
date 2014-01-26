a747202bf0fd3ea199ee6b9ee0fa86984eef66e2
absent1706 <litvinenko1706@gmail.com>
2014-01-26 12:42:21 +0200

    Заработали friendly_url'ы для юзеров. Для этого надо было: 1)Установить гем (версия 4 friendly_id) 2)сделать миграцию и rake db:migrate 3)в модели User добавить   extend FriendlyId   friendly_id :name, use: :slugged , добавить поле slug к attr_accessible и закомментировать строки, связанные с валидацией (чтоб это не мешало пункту 4) 3)User.find_each(&:save) в консоли

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
+
   extend FriendlyId
   friendly_id :name, use: :slugged
+  # def should_generate_new_friendly_id?
+  #   new_record?#генерить новый УРЛ только, когда запись новая. Если мы изменяем существующую, УРЛ останется прежним
+  # end
 
   attr_accessible :name, :email, :password, :password_confirmation,:slug
 
@@ -10,7 +14,7 @@ class User < ActiveRecord::Base
   
   has_many :reverse_relationships, foreign_key: "followed_id",class_name: "Relationship",dependent: :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
-#  has_secure_password
+  has_secure_password
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
@@ -18,9 +22,9 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name,  presence: true, length: { maximum: 50 }
 
- # validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
- # validates :password, presence: true, length: { minimum: 6 }
- # validates :password_confirmation, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 
   def feed
     # Это предварительное решение. См. полную реализацию в "Following users".
