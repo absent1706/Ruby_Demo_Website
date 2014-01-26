46f9f84d763563a1cef6e0de3aff89a149ab626e
absent1706 <litvinenko1706@gmail.com>
2014-01-26 12:39:31 +0200

    Добавил и установил гем. Добавил slug'и. Rails пока руается и не грузит страницу

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
+  attr_accessible :name, :email, :password, :password_confirmation,:slug
 
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
@@ -7,7 +10,7 @@ class User < ActiveRecord::Base
   
   has_many :reverse_relationships, foreign_key: "followed_id",class_name: "Relationship",dependent: :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
-  has_secure_password
+#  has_secure_password
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
@@ -15,9 +18,9 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name,  presence: true, length: { maximum: 50 }
 
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
+ # validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+ # validates :password, presence: true, length: { minimum: 6 }
+ # validates :password_confirmation, presence: true
 
   def feed
     # Это предварительное решение. См. полную реализацию в "Following users".
