11782c916619850ad80306191d9f638b0b62eae8
absent1706 <litvinenko1706@gmail.com>
2014-01-29 18:55:38 +0200

    Новая функция подтверждения регистрации по e-mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,12 +72,15 @@ class UsersController < ApplicationController
     $debug_info=params
     @user = User.where(id: params[:id], signup_confirm_token: params[:token], active: false).first
     if @user
-      @user.update_attribute(:active, true)
-     #@user.save!
-      flash[:success] = "#{@user.name}Account was activated. You can sign in now."
-      redirect_to signin_path
+      if @user.signup_confirm_sent_at > 7.days.ago
+        @user.update_attribute(:active, true)
+        flash[:success] = "Your account was activated. You can sign in now."
+        redirect_to signin_path
+       else
+        redirect_to root_path, notice: "Activation was expired"
+      end
     else
-      redirect_to root_path, notice: "123"
+      redirect_to root_path
     end
   end
 
